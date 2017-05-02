d8fdd619d6724af35090181bb5c6ffb437d1f9bb
Nick Wootton <nick@nwootton.com>
2017-05-02 11:32:38 +0100

    Trying to fix parameter pass-through for dynamic URL

--- node_helper.js
+++ node_helper.js
@@ -27,16 +27,15 @@ var MomentRange = require('moment-range');
 var moment = MomentRange.extendMoment(Moment);
 
 module.exports = NodeHelper.create({
-    start: function () {
+    start: function() {
         console.log('MMM-UKLiveBusStopInfo helper started ...');
     },
 
 
     checkSchedule: function(payload) {
-
         //Is smartTime object populated?
-        if(typeof payload.smartTime !== 'undefined' && payload.smartTime !== null) {
-            console.log("In SmartTime");
+        if (typeof payload.smartTime !== 'undefined' && payload.smartTime !== null) {
+            console.log("In SmartTime for stop " + payload.atco);
 
             //Get today & day of week (DoW)
             var today = moment().format('YYYY-MM-DD');
@@ -49,8 +48,7 @@ module.exports = NodeHelper.create({
             if (payload.smartTime.days.indexOf(DoW) === -1) {
                 //Not requested for today
                 console.log('Not today');
-            }
-            else {
+            } else {
                 //Today is requested
                 console.log('Yay today');
 
@@ -65,45 +63,47 @@ module.exports = NodeHelper.create({
                 var range = moment.range(startRange, endRange);
 
                 //Get now()... NOT in UTC
-                var when  = moment();
+                var when = moment();
 
                 //console.log(when);
 
                 //See if now() is between start and end range
-                if(when.within(range)) {
+                if (when.within(range)) {
                     console.log("NextBuses IS OOOOOONNNNN");
 
                     //Amend url with nextBuses ON
-                    payload.url += "&nextbuses=yes";
-                }
-                else {
+                    //payload.url += "&nextbuses=yes";
+                    payload.url = payload.url.replace(/(nextbuses=)[^\&]+/, '$1' + 'yes');
+
+
+                } else {
                     console.log("Not at this time");
 
                     //Amend url with nextBuses OFF
-                    payload.url += "&nextbuses=no";
+                    //payload.url += "&nextbuses=no";
+                    payload.url = payload.url.replace(/(nextbuses=)[^\&]+/, '$1' + 'no');
                 }
             }
-        }
-        else {
+        } else {
             //Assumes that config is either yes or no for nextBuses
-            console.log("In manual spec");
+            console.log("In manual config for stop " + payload.atco);
         }
 
-        //console.log(payload.url);
+        console.log(payload.atco + ": " + payload.url);
         this.getTimetable(payload.url);
     },
 
-	/* getTimetable()
-	 * Requests new data from TransportAPI.com
-	 * Sends data back via socket on succesfull response.
-	 */
+    /* getTimetable()
+     * Requests new data from TransportAPI.com
+     * Sends data back via socket on succesfull response.
+     */
     getTimetable: function(url) {
         var self = this;
         var retry = true;
 
-        request({url:url, method: 'GET'}, function(error, response, body) {
-            if(!error && response.statusCode == 200) {
-                self.sendSocketNotification('BUS_DATA', {'data': JSON.parse(body), 'url': url});
+        request({ url: url, method: 'GET' }, function(error, response, body) {
+            if (!error && response.statusCode == 200) {
+                self.sendSocketNotification('BUS_DATA', { 'data': JSON.parse(body), 'url': url });
             }
         });
     },
@@ -112,6 +112,7 @@ module.exports = NodeHelper.create({
     socketNotificationReceived: function(notification, payload) {
         if (notification === 'GET_BUSINFO') {
             this.checkSchedule(payload);
+            //console.log(payload);
         }
     }
 
