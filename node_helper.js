d7b400810c62b8345f57474533144c9e6f76d8cf
Nick Wootton <nick@nwootton.com>
2017-07-25 10:25:02 +0100

    Added delay filter to handle issue with info from TransportAPI. Whitespace tidy.

--- node_helper.js
+++ node_helper.js
@@ -10,31 +10,31 @@ var NodeHelper = require('node_helper');
 var request = require('request');
 
 module.exports = NodeHelper.create({
-  start: function () {
-    console.log('MMM-UKLiveBusStopInfo helper started ...');
-  },
+    start: function() {
+        console.log('MMM-UKLiveBusStopInfo helper started ...');
+    },
 
 
-	/* getTimetable()
-	 * Requests new data from TransportAPI.com
-	 * Sends data back via socket on succesfull response.
-	 */
-  getTimetable: function(url) {
-  		var self = this;
-  		var retry = true;
+    /* getTimetable()
+     * Requests new data from TransportAPI.com
+     * Sends data back via socket on succesfull response.
+     */
+    getTimetable: function(url) {
+        var self = this;
+        var retry = true;
 
-      request({url:url, method: 'GET'}, function(error, response, body) {
-        if(!error && response.statusCode == 200) {
-          self.sendSocketNotification('BUS_DATA', {'data': JSON.parse(body), 'url': url});
+        request({ url: url, method: 'GET' }, function(error, response, body) {
+            if (!error && response.statusCode == 200) {
+                self.sendSocketNotification('BUS_DATA', { 'data': JSON.parse(body), 'url': url });
+            }
+        });
+    },
+
+    //Subclass socketNotificationReceived received.
+    socketNotificationReceived: function(notification, payload) {
+        if (notification === 'GET_BUSINFO') {
+            this.getTimetable(payload.url);
         }
-      });
-  	},
-
-  //Subclass socketNotificationReceived received.
-  socketNotificationReceived: function(notification, payload) {
-    if (notification === 'GET_BUSINFO') {
-      this.getTimetable(payload.url);
     }
-  }
 
 });
